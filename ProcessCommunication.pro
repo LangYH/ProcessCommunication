002fa87217c02c26c54feff81188ad5b3f052af0
LangYH <627337658@qq.com>
2014-12-02 16:15:26 +0800

    ProcessCommunication.pro created

---
+++ ProcessCommunication.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-11-29T15:39:21
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = ProcessCommunication
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+HEADERS  += dialog.h
+
+FORMS    += dialog.ui
---
+++ dialog.h
@@ -0,0 +1,34 @@
+﻿#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QProcess>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = 0);
+    ~Dialog();
+
+private slots:
+    void on_browseButton_clicked();
+    void convertImage();
+    void updateOutputTextEdit();
+    void processFinished( int exitCode, QProcess::ExitStatus exitStatus );
+    void processError( QProcess::ProcessError error );
+
+private:
+    QProcess process;
+    QString targetFile;
+
+private:
+    Ui::Dialog *ui;
+};
+
+#endif // DIALOG_H
